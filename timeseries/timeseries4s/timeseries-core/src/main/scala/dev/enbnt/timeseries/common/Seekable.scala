c90f9aceea167d9c15b5831c1fb828e4dada2aaa
enbnt <ian@enbnt.dev>
2023-02-24 21:41:59 -0800

    Remove unused import

--- timeseries/timeseries4s/timeseries-core/src/main/scala/dev/enbnt/timeseries/common/Seekable.scala
+++ timeseries/timeseries4s/timeseries-core/src/main/scala/dev/enbnt/timeseries/common/Seekable.scala
@@ -2,8 +2,6 @@ package dev.enbnt.timeseries.common
 
 import com.twitter.util.Time
 
-import scala.collection.IndexedSeqOps
-
 trait Seekable extends TimeSeriesLike {
 
   def timeIndex(time: Time): Int
